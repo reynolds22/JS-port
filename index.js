4def046394b01e6d35999ea15928ee5f16091902
reynolds22 <zacharysreynolds1830@gmail.com>
2024-01-05 11:50:57 -0500

    finnished most of the function and testing. just need to convert the num index to the words.

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const verb = ['skedaddling', 'flimflamming', 'bamboozle', 'schmooze', 'festooned'];
-const object = ['starbucks', 'dog', 'lamp', 'phone', 'hot dog'];
-const subject = ['Zach', 'JR', 'Sid', 'Nate', 'Chuck'];
+const verb = ['skedaddling', 'loved', 'bamboozled', 'Jumped', 'Impersonated'];
+const object = ['a starbucks', 'a dog', 'a lamp', 'a phone', 'a hot dog'];
+const subject = ['Zach was', 'JR was', 'Sid was', 'Nate was', 'Chuck was'];
 
 const funnySentence = () => {
     let pickedVerb = Math.floor(Math.random()*verb.length);
+    let pickedObject = Math.floor(Math.random()*object.length);
+    let pickedSubject = Math.floor(Math.random()*subject.length);
+
     console.log(pickedVerb);
+    console.log(pickedObject);
+    console.log(pickedSubject);
+    return pickedVerb + pickedObject + pickedSubject;
 };
 
-funnySentence()
+console.log(funnySentence());
